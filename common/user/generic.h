5336c622c963a9d2b3727bac214cbe34e23639a1
spawncpy <spawncpy@gmail.com>
2014-04-12 13:54:07 +0200

    version 1.0
    Updated the shellcode mapping

---
+++ common/user/generic.h
@@ -0,0 +1,11 @@
+#ifndef GENERIC_H
+#define GENERIC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include "sysent.h"
+#include "sys/mman.h"
+
+#endif // GENERIC_H
